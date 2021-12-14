d5e998520c858b81592caeab070837da941fe5ec
Kshitij Tripathi <58387989+Kshitij3003@users.noreply.github.com>
2021-12-14 21:51:03 +0530

    Update jenkins.sh

--- project/jenkins.sh
+++ project/jenkins.sh
@@ -4,3 +4,13 @@ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key 
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update && sudo apt-get install jenkins -y
+
+#Installing ansible
+sudo apt-get update -y
+sudo apt install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update -y
+sudo apt install ansible -y
+sudo apt-get update -y
+cd .ssh/
+ssh-keygen -f id_rsa -t rsa -N ''
